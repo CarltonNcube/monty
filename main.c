3998b6622125d20d0c5c89b3212bdf40944a60c2
CarltonNcube <carltonncube@outlook.com>
2023-08-24 13:47:06 -0700

    Implement the push and pall opcodes

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
-#include "main.h"
-
-/**
- * The main function that executes the Monty interpreter.
- *
- * @param argc Number of command-line arguments.
- * @param argv Array of command-line argument strings.
- * @return Exit status.
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
 
 int main(int argc, char *argv[])
 {
-        if (argc != 2)
-        {
-                fprintf(stderr, "USAGE: %s file\n", argv[0]);
-                return (EXIT_FAILURE);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: %s file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-        }
+    FILE *file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-        stack_t *stack = NULL;
+    stack_t *stack = NULL; // Initialize your stack here
 
-        fclose(file);
+    // Read and process instructions from the file
+    char line[1024];
+    unsigned int line_number = 0;
 
-        return (0);
+    while (fgets(line, sizeof(line), file))
+    {
+        line_number++;
+        // TODO: Parse line to extract opcode and arguments
+        // TODO: Call the appropriate function based on the opcode
+    }
+
+    // TODO: Free memory and close file
+
+    return EXIT_SUCCESS;
 }
---
+++ stack.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * push - Pushes an element onto a stack or queue.
+ * @stack: Pointer to pointer to top or front node.
+ * @line_number: Line number in file.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    int value;
+    stack_t *new_node = malloc(sizeof(stack_t));
+
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    value = atoi(strtok(NULL, "\n\t\r "));
+    new_node->n = value;
+    new_node->prev = NULL;
+
+    if (*stack)
+    {
+        new_node->next = *stack;
+        (*stack)->prev = new_node;
+    }
+    else
+    {
+        new_node->next = NULL;
+    }
+    *stack = new_node;
+}
+
+/**
+ * pall - Prints all the values on the stack or queue.
+ * @stack: Pointer to pointer to top or front node.
+ * @line_number: Line number in file.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current = *stack;
+
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
